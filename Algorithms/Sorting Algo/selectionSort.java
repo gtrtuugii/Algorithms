304a26b1b0304afcbe99109999ee1d6a8fb0a6a6
Tuguldur Gantumur <90265786+gtrtuugii@users.noreply.github.com>
2023-08-18 18:06:05 +1000

    Create selectionSort.java

---
+++ Algorithms/Sorting Algo/selectionSort.java
@@ -0,0 +1,17 @@
+public class SelectionSort extends Sorter {
+
+    public void selectionSort(long[] a) {
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                swap(a, i, minIndex);
+            }
+        }
+    }
+}
