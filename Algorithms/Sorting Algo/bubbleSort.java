fafa01d00945bc26ea4c6d03baf04cddefbccbf5
Tuguldur Gantumur <90265786+gtrtuugii@users.noreply.github.com>
2023-08-18 18:05:46 +1000

    Create bubbleSort.java

---
+++ Algorithms/Sorting Algo/bubbleSort.java
@@ -0,0 +1,13 @@
+public class BubbleSort extends Sorter {
+
+    public void bubbleSort(long[] a) {
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    swap(a, j, j + 1);
+                }
+            }
+        }
+    }
+}
